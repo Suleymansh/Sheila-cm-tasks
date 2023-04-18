2a3d2fa4ae728bc0f7b1ad2dcb80004a8939e861
sheila <hasan@webluna.az>
2023-04-18 09:08:37 +0400

    add Task4

---
+++ html_css/Task4/app.js
@@ -0,0 +1,31 @@
+//  let a = 5;
+//  let b = 2;
+//  let perimetr=2*(a+b);
+//  console.log(perimetr);
+
+
+// let  naMe = 'Sheila';
+// let suRname= 'Suleyamn';
+// let nasur = naMe + ' ' +suRname;
+// console.log(nasur);
+
+const date = new Date();
+const year =  2000;
+console.log(date.getFullYear()-year);
+// let nowyear = prompt("Please enter your year");
+// let year = prompt("Please enter your  yeras ");
+// let age = nowyear - year;
+// console.log(age);3
+
+
+// 106.22 aldigi qiymet
+
+// let kilometers = prompt("eded dax et:");
+// let miles =  0.6214;
+
+// let change = (kilometers*miles);
+// console.log(change.toFixed(1));
+
+
+
+
