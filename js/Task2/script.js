be33dda47cc345f43e98263ef393f29a88769c31
sheila <hasan@webluna.az>
2023-04-20 00:28:57 +0400

    add Task3

---
+++ js/Task2/script.js
@@ -0,0 +1,11 @@
+let a=prompt( " sozu elave et:")
+ 
+if (a === "user") {
+  console.log("xos geldin istifadeci ");
+} else if (a === "admin") {
+  console.log("xos geldin cenab admin");
+} else if (a === "guest") {
+  console.log("xos geldin qonaq");
+} else {
+  console.log(" diger hallar");
+}
---
+++ js/Task3/script.js
@@ -0,0 +1,111 @@
+// let a = prompt(" sozu elave et:");
+
+// if (a === "user") {
+//   console.log("xos geldin istifadeci ");
+// } else if (a === "admin") {
+//   console.log("xos geldin cenab admin");
+// } else if (a === "guest") {
+//   console.log("xos geldin qonaq");
+// } else {
+//   console.log(" diger hallar");
+// }
+
+// let a = prompt(" sozu elave et:");
+
+// switch (a) {
+//   case "user":
+//     console.log("xos geldin istifadeci ");
+//     break;
+//   case "admin":
+//     console.log("xos geldin cenab admin");
+//     break;
+//   default:
+//     console.log("diger hallar");
+// }
+// let a = prompt("eded elave et");
+
+// if (a%3===0 && a%5===0) {
+//     console.log( "FizzBuzz");
+
+// }
+
+// else if (a % 3 === 0) {
+//     console.log("Fizz");
+// }
+
+// else if (a%5===0) {
+//   console.log("Buzz")
+// }
+// else {
+//     console.log( a);
+// }
+
+//  let num = prompt("Enter a number:");
+
+//  if (num>=10 && num<=99) {
+//     console.log( "The number is 2 digits");
+//  }
+//  else{
+// console.log("Dier hallar")
+//  }
+
+// let num = prompt("Enter a number:");
+// if (num % 2 === 0 && num % 5 === 0) {
+//   console.log("Bu istediyim ededdir");
+// } else {
+//   console.log(" diger hallar");
+// }
+
+// let a =  Number(prompt("Enter a number:"));
+// let b =  Number(prompt("Enter a number:"));
+// let c =  Number(prompt("Enter a number:"));
+
+// let total =a+b+c;
+//  if (total>100) {
+//     console.log( " 100-u kecdi");   
+//  }
+//  else{
+//     console.log("diger hallar"  );
+//  }
+// Prompt user to enter a number
+// const num = prompt("Enter a number:");
+
+
+// const sqrt = Math.sqrt(num);
+
+// console.log(sqrt);
+
+
+//  let num = prompt("Enter a number:");
+
+// if (num < 0) {
+//    console.log("eded menfidir");
+//  } else if ( num>0) {
+//    console.log("eded musbetdir");
+//  }
+//  else{
+//     console.log(' diger hallar');
+//  }   
+
+//   let num = prompt("Enter a number:");
+//   if (num%2===0) {
+//     console.log("cut");
+//   } else{
+//     console.log(" tek")
+//   }
+
+   let a = prompt("Enter a number:");5
+   let b = prompt("Enter a number:");10
+   let c = prompt("Enter a number:");15
+   if (a>=b && a>=c) {
+    console.log( ` a boyukdur ${a}`);
+    
+   }
+   else if (b>=a &&b>=c){
+   console.log(` b boyukdur ${b}`);
+   }
+   else{
+      console.log(` c boyukdur ${c}`);
+   }
+
+
