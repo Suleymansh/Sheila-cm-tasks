7f8125a228143ba3122f528ae1f98d9af5f46e05
sheila <hasan@webluna.az>
2023-05-11 15:09:33 +0400

    add Task3

---
+++ exam/Task3/app.js
@@ -0,0 +1,12 @@
+// 5. Show the count of the both odd and even numbers from array.
+// let numbers = [1,4,7,8,3,5,0,2,4,6,12,15,18,46,45,48,97,84]
+
+// For example, countEvensAndOdds(numbers);   // evens: 12, odds: 13
+
+//   const countEvensAndOdds=(numbers)=>{
+//     const evens = numbers.filter((elem)=>elem%2===0 && elem !==0)
+//     const odds = numbers.filter((elem)=> elem%2 !==0)
+
+//       console.log(`evens: ${evens}, odds: ${odds.length}`);
+//   }
+// countEvensAndOdds([ 1, 4, 7, 8, 3, 5, 0, 2, 4, 6, 12, 15, 18, 46, 45, 48, 97, 84,]);
