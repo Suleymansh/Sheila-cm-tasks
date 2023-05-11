b9dfb8da8d323bf3a0533a4884aec7467900cae1
sheila <hasan@webluna.az>
2023-05-11 15:06:54 +0400

    add Task2

---
+++ exam/Task2/app.js
@@ -0,0 +1,21 @@
+
+// 3. Show number's dividends from 100 to 1000.
+// Input | Output
+// 50      | 100, 150, 200, 250, 300 ... 950
+// 70      | 140 , 210, 280, 980 ...
+// 85      | 170, 255, 340, ... 935
+
+
+// const divisors=(number)=> {
+//      let results = [];
+//   for (let i = 100; i <= 1000; i++) {
+//     if (i % number === 0) {
+//       results.push(i);
+//     }
+//   } 
+//   return results;
+// }
+
+// console.log(divisors(50)); // Output: [100, 150, 200, 250, ..., 950]
+// console.log(divisors(70)); // Output: [140, 210, 280, ..., 980]
+// console.log(divisors(85)); // Output: [170, 255, 340, ..., 935]
